427088b27d38007cec8324d22e9c42f699533d37
Pierre Quignon <p.quignon.pro@gmail.com>
2023-01-20 10:42:49 +0100

    added some style to banner and cart

--- packages/cra-template/template/src/compoments/Banner.js
+++ packages/cra-template/template/src/compoments/Banner.js
@@ -1,7 +1,11 @@
 import '../styles/Banner.css';
 
 function Banner() {
-  return <div className="lmj-banner">La maison jungle</div>;
+  return (
+    <div className="lmj-banner">
+      <h1>La maison jungle</h1>
+    </div>
+  );
 }
 
 export default Banner;
