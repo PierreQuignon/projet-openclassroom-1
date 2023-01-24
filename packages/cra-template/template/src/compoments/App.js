16a99a26614aa6d2fc43b2af03fa78a3b2196ae8
Pierre Quignon <p.quignon.pro@gmail.com>
2023-01-24 20:13:10 +0100

    try to use useState

--- packages/cra-template/template/src/compoments/App.js
+++ packages/cra-template/template/src/compoments/App.js
@@ -1,17 +1,20 @@
 import Banner from './Banner';
-import Cart from './Cart';
+import logo from '../assets/logo.png';
+// import Cart from './Cart'
+import Footer from './Footer';
 import ShoppingList from './ShoppingList';
-import CareScale from './CareScale';
-import PlantItem from './PlantItem';
 
 function App() {
   return (
     <div>
-      <Banner />
-      <Cart />
+      <Banner>
+        <img src={logo} alt="La maison jungle" className="lmj-logo" />
+        <h1 className="lmj-title">La maison jungle</h1>
+      </Banner>
+
+      {/* <Cart /> */}
       <ShoppingList />
-      <CareScale />
-      <PlantItem />
+      <Footer />
     </div>
   );
 }
