1573f9be484776fc35b7e95bbc78d65f188db61f
Pierre Quignon <p.quignon.pro@gmail.com>
2023-01-26 16:28:09 +0100

    try another time to fix a problem with useState

--- packages/cra-template/template/src/compoments/App.js
+++ packages/cra-template/template/src/compoments/App.js
@@ -1,7 +1,6 @@
 import Banner from './Banner';
 import logo from '../assets/logo.png';
-// import Cart from './Cart'
-import Footer from './Footer';
+import Cart from './Cart';
 import ShoppingList from './ShoppingList';
 
 function App() {
@@ -11,10 +10,8 @@ function App() {
         <img src={logo} alt="La maison jungle" className="lmj-logo" />
         <h1 className="lmj-title">La maison jungle</h1>
       </Banner>
-
-      {/* <Cart /> */}
+      <Cart />
       <ShoppingList />
-      <Footer />
     </div>
   );
 }
